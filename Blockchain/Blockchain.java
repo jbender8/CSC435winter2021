6161b45c329e62ae7216a722b371f47737679640
Jessica Bender <jessicabednder18@gmail.com>
2021-02-22 11:44:26 -0600

    started checklist + added java file

---
+++ Blockchain/Blockchain.java
@@ -0,0 +1,35 @@
+/*--------------------------------------------------------
+
+1. Name / Date: Jessica Bender / version 1 - 2/22/2021
+
+2. Java version used, if not the official version for the class:
+
+My Java:
+java version "9.0.4"
+Java(TM) SE Runtime Environment (build 9.0.4+11)
+Java HotSpot(TM) 64-Bit Server VM (build 9.0.4+11, mixed mode)
+
+3. Precise command-line compilation examples / instructions:
+> javac JokeClient.java
+
+4. Precise examples / instructions to run this program:
+In separate shell windows:
+
+> java JokeServer
+> java JokeClient
+> java JokeClientAdmin
+
+5. List of files needed for running the program.
+
+ a. 
+ b. JokeServer.java
+ c. JokeClient.java
+ d. JokeClientAdmin.java
+
+6. Notes:
+
+
+
+----------------------------------------------------------*/
+
+//Code Starts Here:
